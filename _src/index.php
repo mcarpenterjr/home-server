42020af9e9c5a8330f2b45881a214fb4ce59d17c
Mark N Carpenter Jr <marksthings24@gmail.com>
2017-02-24 23:06:43 -0500

    INITIAL COMMIT

---
+++ _src/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Home Server</title>
+
+    <!-- Bootstrap -->
+    <link href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-3">
+        <div class="panel">
+          <div class="panel-heading">
+            <h3 class="panel-title">Sample Bootstrap Panel</h3>
+          </div>
+          <div class="panel-body">
+            Cool Info about our web server will show up here.
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="lib/jquery/jquery-3.1.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
