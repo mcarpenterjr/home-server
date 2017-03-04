e8956390378ad7c18e930df342a7f7fff30e00d1
Mark N Carpenter Jr <marksthings24@gmail.com>
2017-03-04 11:21:08 -0500

    5day 3hour Forecast

--- _src/js/index.js
+++ _src/js/index.js
@@ -1,10 +1,35 @@
 var oWDKey = '8968f560767b83ff6972db1aec284389';
 var oWDFCUrl = 'http://api.openweathermap.org/data/2.5/weather';
+var oWDFC5DUrl = 'http://api.openweathermap.org/data/2.5/forecast';
 
-$.getJSON(oWDFCUrl, {
-  id: 5280935,
-  units: 'imperial',
-  APPID: oWDKey,
+function model() {
+  var self = this;
+
+  self.lat = ko.observable();
+  self.long = ko.observable();
+  self.townShip = ko.observable();
+
+  self.fiveDay = ko.observableArray();
+
+}
+var model = new model();
+
+function fiveDay() {
+  $.getJSON(oWDFC5DUrl, {
+    id: 5280935,
+    units: 'imperial',
+    APPID: oWDKey,
+  }, function(json, textStatus) {
+    console.log(json.list);
+    model.fiveDay(json.list);
+  });
+}
+
+function weatherNow() {
+  $.getJSON(oWDFCUrl, {
+    id: 5280935,
+    units: 'imperial',
+    APPID: oWDKey,
   },
   function(json, textStatus) {
     console.log(json);
@@ -14,4 +39,12 @@ $.getJSON(oWDFCUrl, {
     $('.conditions').append('<li>Pressure: ' + json.main.pressure + '<sup>bar</sup></li>');
     $('.conditions').append('<li>Temperature: ' + json.main.temp_max + '<sup>HIGH</sup></li>');
     $('.conditions').append('<li>Temperature: ' + json.main.temp_min + '<sup>LOW</sup></li>');
-});
+  });
+}
+
+function init() {
+  weatherNow();
+  fiveDay();
+}
+
+ko.applyBindings(model);
